abe13dfbb92cac58c679146982145e5c05d453d9
ge54luq <ge54luq@mytum.de>
2021-10-27 16:17:56 +0200

    Implement handlePastEvents()

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -107,8 +107,8 @@ public class Application {
                 JSONObject event = past.getJSONObject(i);
                 Long timeStamp = event.getLong("date_unix");
                 Date date = new Date(timeStamp*1000);
-                int crewAmount = event.getJSONArray("crew").length();
-                System.out.println(count + ". " + "The event launched on " + date + " with " + crewAmount + "people on board.");
+                System.out.print(count + ". " + "The launch was on " + date + " with ");
+                printCrewAmount(event.getJSONArray("crew").length());
                 if (event.has("details") && event.get("details") instanceof String) {
                     System.out.println("The description to this event is:\n" + event.getString("details"));
                 }
@@ -120,12 +120,134 @@ public class Application {
                 }
                 count++;
             }
-            System.out.println("Do you want to get more information about any of those events?\n" +
-                    "Type YES if you do:)");
-            if (scanner.nextLine().equalsIgnoreCase("yes")) {
-                System.out.println("Please type in the number of event.");
+            action += ":getNumber";
+            while (action.startsWith("past:getNumber")) {
+                System.out.println("Please type in the number of event you want to learn more about.\n" +
+                        "If you want to exit this section please type in \"EXIT\"");
+                String idLine = scanner.nextLine();
+                if (idLine.matches("\\d+")) {
+                    int id = Integer.parseInt(idLine);
+                    if (id > count || id < 1) {
+                        System.out.println("Please enter the right number.");
+                        continue;
+                    }
+
+                    JSONObject event = past.getJSONObject(past.length() - id);
+                    Long timeStamp = event.getLong("date_unix");
+                    Date date = new Date(timeStamp*1000);
+                    System.out.print("The launch was on " + date + " with ");
+                    getEventData(event);
+                } else if (idLine.equals("EXIT")) {
+                    action = "";
+                    System.out.println("Now you can type in the next command");
+                }
             }
 
+        } catch (IOException error) {
+            System.out.println("Sorry, something went wrong. Please try again later.");
+        }
+    }
+
+    private static void printCrewAmount(int crewAmount) {
+        if (crewAmount == 0)
+            System.out.println("no people ob board.");
+        else if (crewAmount == 1)
+            System.out.println("1 person on board.");
+        else
+            System.out.println(crewAmount + " people on board.");
+    }
+
+    private static void getEventData(JSONObject event) {
+        try {
+            JSONArray crew = event.getJSONArray("crew");
+            int crewAmount = crew.length();
+            printCrewAmount(crewAmount);
+
+            if (event.has("details") && event.get("details") instanceof String) {
+                System.out.println("The description to this event is:\n" + event.getString("details"));
+            }
+
+            if (event.has("links") && event.get("links") instanceof JSONObject) {
+                JSONObject links = event.getJSONObject("links");
+                if (links.has("webcast") && links.get("webcast") instanceof String)
+                    System.out.println("Here is the link to webcast: " + new URL(links.getString("webcast")));
+
+                if (links.has("wikipedia") && links.get("wikipedia") instanceof String)
+                    System.out.println("Here is the link to wikipedia page: " + new URL(links.getString("wikipedia")));
+
+                if (links.has("article") && links.get("article") instanceof String)
+                    System.out.println("Here is the link to an article: " + new URL(links.getString("article")));
+            }
+            action += ":choosing";
+            System.out.println("If you want to get more information about the rocket please type in \"rocket\".");
+            System.out.println("If you want to get more information about the crew please enter \"crew\".");
+            System.out.println("If you want to get more information about the launchpad please type in \"launchpad\"");
+            System.out.println("If you want to abort this section type \"EXIT\"");
+            while (action.contains(":choosing") && scanner.hasNext()) {
+                switch (scanner.nextLine()) {
+                    case "rocket":
+                        String rocketId = "";
+                        if (event.has("rocket") && event.get("rocket") instanceof String)
+                            rocketId = event.getString("rocket");
+                        if (rocketId.equals("")) {
+                            System.out.println("Sorry, information about the rocket is missing.\n" +
+                                    "Please try again later.");
+                            break;
+                        }
+                        String rocketInfo = APIClient.getRocketInfo(rocketId);
+                        JSONObject rocket = new JSONObject(rocketInfo);
+                        String nameRocket = rocket.getString("name");
+                        String type = rocket.getString("type");
+                        String firstFlight = rocket.getString("first_flight");
+                        System.out.println("The name of the " + type + " is " + nameRocket + "\n" +
+                                "and its first flight was on " + firstFlight + ".");
+                        if (rocket.has("description") && rocket.get("description") instanceof String) {
+                            String description = rocket.getString("description");
+                            System.out.println("Provided description is:\n" + description);
+                        }
+                        if (rocket.has("flickr_images") && rocket.get("flickr_images") instanceof JSONArray) {
+                            String flickrImage = ((JSONArray) rocket.get("flickr_images")).getString(0);
+                            System.out.println("Here is a link to an image: " + new URL(flickrImage));
+                        }
+                        break;
+                    case "crew":
+                        if (crewAmount == 0) {
+                            System.out.println("Oops, it seems like there is no information about the crew. Please try another time.");
+                            break;
+                        }
+                        for (int i = 0; i < crew.length(); i++) {
+                            String memberId = crew.getJSONObject(i).getString("crew");
+                            JSONObject member = new JSONObject(APIClient.getCrewInfo(memberId));
+                            String nameCrew = member.getString("name");
+                            String role = crew.getJSONObject(i).getString("role");
+                            System.out.println(nameCrew + " is the " + role + ".");
+
+                            if (member.has("image") && member.get("image") instanceof String)
+                                System.out.println("Here is a link to the photo: " + new URL(member.getString("image")));
+
+                            if (member.has("wikipedia") && member.get("wikipedia") instanceof String)
+                                System.out.println("Here is a link to the wikipedia page: " + new URL(member.getString("wikipedia")));
+                        }
+                        break;
+                    case "launchpad":
+                        String launchpadId = "";
+                        if (event.has("launchpad") && event.get("launchpad") instanceof String)
+                            launchpadId = event.getString("launchpad");
+                        if (launchpadId.equals("")) {
+                            System.out.println("Sorry, information about the launchpad is missing.\n" +
+                                    "Please try again later.");
+                            break;
+                        }
+                        String launchpadInfo = APIClient.getLaunchpadInfo(launchpadId);
+                        JSONObject launchpad = new JSONObject(launchpadInfo);
+                        String nameLaunchpad = launchpad.getString("full_name");
+                        int success = launchpad.getJSONArray("launches").length();
+                        System.out.println(nameLaunchpad + " has already " + success + " succeeded launches!");
+                        break;
+                    case "EXIT":
+                        action = action.substring(0, action.lastIndexOf(":"));
+                }
+            }
         } catch (IOException error) {
             System.out.println("Sorry, something went wrong. Please try again later.");
         }
@@ -151,93 +273,12 @@ public class Application {
             }
             Long timeStamp = nextEvent.getLong("date_unix");
             Date date = new Date(timeStamp*1000);
-
-            int crewAmount = nextEvent.getJSONArray("crew").length();
-
-            System.out.println("Next launch is on " + date + "with " + crewAmount + " people on board.");
-
-            if (nextEvent.has("details") && nextEvent.get("details") instanceof String) {
-                System.out.println("The description to this event is:\n" + nextEvent.getString("details"));
-            }
-
-            if (nextEvent.has("links") && nextEvent.get("links") instanceof JSONObject) {
-                JSONObject links = nextEvent.getJSONObject("links");
-                if (links.has("webcast") && links.get("webcast") instanceof String)
-                    System.out.println("Here is the link to webcast: " + new URL(links.getString("webcast")));
-
-                if (links.has("wikipedia") && links.get("wikipedia") instanceof String)
-                    System.out.println("Here is the link to wikipedia page: " + new URL(links.getString("wikipedia")));
-
-                if (links.has("article") && links.get("article") instanceof String)
-                    System.out.println("Here is the link to an article: " + new URL(links.getString("article")));
-            }
-
-            System.out.println("Do you want to get more information about this launch?\n" +
-                    "Type YES if you do:)");
-            if (scanner.nextLine().equalsIgnoreCase("yes")) {
-                action += ":choosing";
-                System.out.println("If you want to get more information about the rocket please type in \"rocket\".");
-                if (crewAmount > 0)
-                    System.out.println("If you want to get more information about the crew please enter \"crew\".");
-                System.out.println("If you want to abort this section type EXIT");
-            } else {
-                action = "";
-                System.out.println("Now you can type in the next command");
-                return;
-            }
-            while (scanner.hasNext() && action.equals("upcoming:choosing")) {
-                switch (scanner.nextLine()) {
-                    case "rocket" :
-                        String rocketId = "";
-                        if (nextEvent.has("rocket") && nextEvent.get("rocket") instanceof String)
-                            rocketId = nextEvent.getString("rocket");
-                        if (rocketId.equals("")) System.out.println("Sorry, information about the rocket is missing.\n" +
-                                "Please try again later.");
-                        else {
-                            String rocketInfo = APIClient.getRocketInfo(rocketId);
-                            JSONObject rocket = new JSONObject(rocketInfo);
-                            String name = rocket.getString("name");
-                            String type = rocket.getString("type");
-                            String firstFlight = rocket.getString("first_flight");
-                            System.out.println("The name of the " + type + " is " + name + "\n" +
-                                    "and its first flight was on " + firstFlight + ".");
-                            if (rocket.has("description") && rocket.get("description") instanceof String) {
-                                String description = rocket.getString("description");
-                                System.out.println("Provided description is:\n" + description);
-                            }
-                            if (rocket.has("flickr_images") && rocket.get("flickr_images") instanceof JSONArray) {
-                                String flickrImage = ((JSONArray) rocket.get("flickr_images")).getString(0);
-                                System.out.println("Here is a link to an image: " + new URL(flickrImage));
-                            }
-                        }
-                        break;
-                    case "crew" :
-                        JSONArray crew = nextEvent.getJSONArray("crew");
-                        for (int i = 0; i < crew.length(); i++) {
-                            String memberId = crew.getJSONObject(i).getString("crew");
-                            JSONObject member = new JSONObject(APIClient.getCrewInfo(memberId));
-                            String name = member.getString("name");
-                            String role = crew.getJSONObject(i).getString("role");
-                            System.out.println(name + " is the " + role + ".");
-
-                            if (member.has("image") && member.get("image") instanceof String)
-                                System.out.println("Here is a link to the photo: " + new URL(member.getString("image")));
-
-                            if (member.has("wikipedia") && member.get("wikipedia") instanceof String)
-                                System.out.println("Here is a link to the wikipedia page: " + new URL(member.getString("wikipedia")));
-                        }
-                        break;
-                    case "EXIT" :
-                        action = "";
-                        System.out.println("Now you can type in the next command");
-                }
-            }
+            System.out.print("Next launch is on " + date + " with ");
+            getEventData(nextEvent);
+            action = "";
+            System.out.println("Now you can type in the next command");
         } catch (IOException error) {
             System.out.println("Sorry, something went wrong. Please try again later.");
         }
-    }
-
-    private static void getEventData(JSONObject event) {
-
     }
 }
